b1a5e344fc7e33b159afb5e6d6900b6670772973
2kram <al.awad7074@gmail.com>
2022-10-11 00:55:17 +0300

    worked on /apps/new-version-available req tests

---
+++ backend/spec/requests/apps_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+require 'json'
+
+RSpec.describe "Apps", type: :request do
+  andorid_app_version = AppsHelper::APP_VERSION["android"]
+  ios_app_version = AppsHelper::APP_VERSION["ios"]
+  
+  describe "GET /apps/new-version-available" do
+    it "is return false with android" do
+      get "/v1/apps/new-version-available", :params => {
+        "app_version": andorid_app_version,
+        "phone_os": "android"
+      }
+
+      expect(JSON.parse(response.body)["status"]).to equal(false)
+    end
+    
+    it "is return false with ios" do
+      get "/v1/apps/new-version-available", :params => {
+        "app_version": ios_app_version,
+        "phone_os": "ios"
+      }
+
+      expect(JSON.parse(response.body)["status"]).to equal(false)
+    end
+
+    it "is return false with android & old data" do
+      get "/v1/apps/new-version-available", :params => {
+        "app_version": andorid_app_version + 1,
+        "phone_os": "android"
+      }
+
+      expect(JSON.parse(response.body)["status"]).to equal(false)
+    
+    end
+    
+    it "is return false with ios & old data" do
+      get "/v1/apps/new-version-available", :params => {
+        "app_version": ios_app_version + 1,
+        "phone_os": "ios"
+      }
+
+      expect(JSON.parse(response.body)["status"]).to equal(false)
+    end
+    
+    it "is return true with android" do
+      get "/v1/apps/new-version-available", :params => {
+        "app_version": andorid_app_version - 1,
+        "phone_os": "android"
+      }
+
+      expect(JSON.parse(response.body)["status"]).to equal(true)
+    end
+    
+    it "is return true with ios" do
+      get "/v1/apps/new-version-available", :params => {
+        "app_version": ios_app_version - 1,
+        "phone_os": "ios"
+      }
+
+      expect(JSON.parse(response.body)["status"]).to equal(true)
+    end
+  end
+end
