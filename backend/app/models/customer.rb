c482a6f37161c8390feadfa7f6e5555fc37201a9
2kram <al.awad7074@gmail.com>
2022-10-11 19:36:15 +0300

    worked on generate verification code for customer

--- backend/app/models/customer.rb
+++ backend/app/models/customer.rb
@@ -1,4 +1,5 @@
 class Customer < ApplicationRecord
+  include Verification
   include PublicIdGenerator
 
   before_create :set_phone_verification_status
@@ -15,11 +16,16 @@ class Customer < ApplicationRecord
     verified: 1
   }, _prefix: :phone
 
+  enum verification_code_permission: {
+    "customer_verification": 0
+  }, _prefix: :verification_code_permission
+
   validates :public_id, presence: true, uniqueness: true
   validates :name, presence: true
   validates :country, presence: true, inclusion: { in: COUNTRIES }
   validates :phone_number, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 8 }
+  validates :password, presence: true, length: { minimum: 8 }, on: :create
+  validates :verification_code_permission, inclusion: { in: Customer.verification_code_permissions  }
 
   private
   def set_phone_verification_status
---
+++ backend/app/models/customer/verification.rb
@@ -0,0 +1,13 @@
+module Customer::Verification
+  require 'securerandom'
+
+  def generate_verification_code(permission:)
+    code = SecureRandom.random_number(100000..999999)
+
+    self.update(
+      verification_code: code,
+      verification_code_permission: permission,
+      verification_code_created_at: Time.now
+    )
+  end
+end
