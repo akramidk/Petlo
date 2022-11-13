e500f63b505471cfb73a3e974b276277d690d954
2kram <al.awad7074@gmail.com>
2022-11-14 00:02:48 +0300

    fixed somethings in pets model

--- backend/app/models/pet.rb
+++ backend/app/models/pet.rb
@@ -3,14 +3,13 @@ class Pet < ApplicationRecord
 
   belongs_to :customer
 
-  # enum gender: {
-  #   "male": 0,
-  #   "female": 1
-  # }, _prefix: :gender
+  enum gender: {
+    "male": 0,
+    "female": 1
+  }
 
   validates :public_id, presence: true, uniqueness: true
   validates :customer_id, presence: true
   validates :name, presence: true
   validates :kind, presence: true, inclusion: { in: CONSTANTS::PETS.keys }
-  # validates :breed, presence: true, inclusion: { in: CONSTANTS::PETS[self.kind].breeds.keys }
 end
