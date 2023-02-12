ce902c5d724f8aaa4f27882ef2f389588045c87d
2kram <al.awad7074@gmail.com>
2023-02-12 08:38:23 +0300

    handled if item or variant not available on customer country

--- backend/app/helpers/carts_helper/add_item.rb
+++ backend/app/helpers/carts_helper/add_item.rb
@@ -4,10 +4,24 @@ module CartsHelper::AddItem
         raise(RuntimeError, 3004000) if !cart
         raise(RuntimeError, 3004001) if cart.created_at + 24.hours < Time.now
 
-        item = Item.find_by(public_id: item_id)
+        item = Item.joins(
+            :availabilities
+        ).where(availabilities: {
+            country: customer.country,
+            value: true
+        }).find_by(
+            public_id: item_id
+        )
         raise(RuntimeError, 3004002) if !item
 
-        variant = Variant.find_by(public_id: variant_id)
+        variant = item.variants.joins(
+            :availabilities
+        ).where(availabilities: {
+            country: customer.country,
+            value: true
+        }).find_by(
+            public_id: variant_id
+        )
         raise(RuntimeError, 3004003) if !variant
 
         CartItem.create!(
