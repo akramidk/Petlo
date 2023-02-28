7a16ae7fb561a3bd8b8403e3411e06d45af2a88e
2kram <al.awad7074@gmail.com>
2023-02-28 21:15:53 +0300

    eas build things

--- frontend/mobile/app.config.ts
+++ frontend/mobile/app.config.ts
@@ -3,10 +3,16 @@ export default {
     name: "Petlo",
     slug: "Petlo",
     scheme: "acme",
+    android: {
+      package: "com.petlo",
+    },
     web: {
       bundler: "metro",
     },
     extra: {
+      eas: {
+        projectId: "184da1b4-f7ae-47f9-84e1-695fca3a70f0",
+      },
       API_URL: process.env.API_URL,
     },
   },
