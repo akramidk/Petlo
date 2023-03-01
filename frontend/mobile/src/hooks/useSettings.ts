9dfa9067c4ef0f1a743bd7dddc9aed6bb46dbc4c
2kram <al.awad7074@gmail.com>
2023-03-01 20:56:23 +0300

    fixed rtl problem on android

--- frontend/mobile/src/hooks/useSettings.ts
+++ frontend/mobile/src/hooks/useSettings.ts
@@ -1,16 +1,25 @@
 import { useMemo, useState } from "react";
 import { languages } from "../types";
+import { I18nManager } from "react-native";
+
+type direction = "ltr" | "rtl";
+enum languagesDirection {
+  "en" = "ltr",
+  "arMasculine" = "rtl",
+  "arFeminine" = "rtl",
+}
 
 const useSettings = () => {
+  //we are handling ltr and rtl on our own
+  I18nManager.allowRTL(false);
+  I18nManager.forceRTL(false);
+
   const [language, setLanguage] = useState<languages>("en");
-  const [direction, setDirection] = useState<"ltr" | "rtl">("ltr");
+  const [direction, setDirection] = useState<direction>("ltr");
 
   useMemo(() => {
-    if (language === "en") {
-      setDirection("ltr");
-    } else {
-      setDirection("rtl");
-    }
+    const direction = languagesDirection[language];
+    setDirection(direction);
   }, [language]);
 
   return { language, setLanguage, direction };
