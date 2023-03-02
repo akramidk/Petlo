ebb978c49b73745a50413ae57afbb7b4dca0db2b
2kram <al.awad7074@gmail.com>
2023-03-02 23:01:51 +0300

    added select-language page

---
+++ frontend/mobile/app/select-language.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const SelectLanguage = () => {
+  return (
+    <View>
+      <Text>Hello to select-language</Text>
+    </View>
+  );
+};
+
+export default SelectLanguage;
