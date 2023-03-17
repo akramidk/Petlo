5ba8d843d253472eaf6c944d5de00749db60f674
2kram <al.awad7074@gmail.com>
2023-03-17 18:33:37 +0300

    backend

--- backend/app/helpers/customers_helper/reset_password.rb
+++ backend/app/helpers/customers_helper/reset_password.rb
@@ -1,7 +1,7 @@
 module CustomersHelper::ResetPassword
     def reset_password(customer:, password:)
         Customer.validates_password(password: password)
-        customer.update!(password: password)
+        customer.update!(phone_verification_status: "verified", password: password)
 
         session_token = SessionToken.generate(
             public_id: customer.public_id,
