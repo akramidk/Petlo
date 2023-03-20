696fdf60bb1d597ea1b313c0b6355a78b2d8b817
2kram <al.awad7074@gmail.com>
2023-03-20 22:36:39 +0100

    informations

---
+++ frontend/mobile/app/account/informations/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Informations = () => {
+  return (
+    <View>
+      <Text>informations</Text>
+    </View>
+  );
+};
+
+export default Informations;
