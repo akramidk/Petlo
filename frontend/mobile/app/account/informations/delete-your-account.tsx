49e78d22cb7c1c2d09bbb07368f6368bb0443cd3
2kram <al.awad7074@gmail.com>
2023-03-25 13:18:04 +0300

    t

--- frontend/mobile/app/account/informations/delete-your-account.tsx
+++ frontend/mobile/app/account/informations/delete-your-account.tsx
@@ -1,22 +1,24 @@
 import { useRouter } from "expo-router";
 import { useState } from "react";
 import { PageStructure } from "../../../src/components/organisms";
+import { useTranslationsContext } from "../../../src/hooks";
 
 const DeleteYourAccount = () => {
   const router = useRouter();
   const [step, setStep] = useState(1);
+  const { t } = useTranslationsContext();
 
   if (step === 1) {
     return (
       <PageStructure
-        title="Delete Your Account"
-        helperText="If you delete your account there is no going back, you can't login or use your account once you delete it, are you sure about this?"
+        title={t("DELETE_YOUR_ACCOUNT_STEP_1_TITLE")}
+        helperText={t("DELETE_YOUR_ACCOUNT_STEP_1_HELPER_TEXT")}
         button={{
-          value: "Yes",
+          value: t("DELETE_YOUR_ACCOUNT_STEP_1_BUTTON"),
           onClick: () => setStep(2),
         }}
         link={{
-          value: "Cancel",
+          value: t("DELETE_YOUR_ACCOUNT_CANCEL_BUTTON"),
           onClick: router.back,
           valueCN: "text-[#E64848]",
         }}
@@ -28,7 +30,7 @@ const DeleteYourAccount = () => {
     return (
       <PageStructure
         title="Delete Your Account 2"
-        helperText="If you delete your account there is no going back, you can't login or use your account once you delete it, are you sure about this?"
+        helperText="If you delete your account there is no going back, you can't sign in or use your account once you delete it, are you sure about this?"
         button={{
           value: "Yes",
           onClick: () => console.log("ddd"),
