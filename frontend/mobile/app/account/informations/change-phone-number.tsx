f9399700f5a094b6802cd0c45e62c8b8a515eb53
2kram <al.awad7074@gmail.com>
2023-03-25 16:30:02 +0300

    ChangePhoneNumber

---
+++ frontend/mobile/app/account/informations/change-phone-number.tsx
@@ -0,0 +1,7 @@
+import { PageStructure } from "../../../src/components/organisms";
+
+const ChangePhoneNumber = () => {
+  return <PageStructure title="Change Your Phone Number"></PageStructure>;
+};
+
+export default ChangePhoneNumber;
