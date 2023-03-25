81df668fae709554d66eb3ea8717ac1cc40dbcb9
2kram <al.awad7074@gmail.com>
2023-03-25 15:31:02 +0300

    ChangeName

--- frontend/mobile/app/account/informations/change-name.tsx
+++ frontend/mobile/app/account/informations/change-name.tsx
@@ -1,10 +1,35 @@
-import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import { PageStructure } from "../../../src/components/organisms";
+import { useTranslationsContext } from "../../../src/hooks";
+import { Filed } from "../../../src/components/atoms";
 
 const ChangeName = () => {
+  const router = useRouter();
+  const { t } = useTranslationsContext();
+
+  const [name, setName] = useState("");
+
   return (
-    <View>
-      <Text>Change Text</Text>
-    </View>
+    <PageStructure
+      title={t("CHANGE_NAME__TITLE")}
+      button={{
+        value: t("CHANGE_NAME__CHANGE_BUTTON"),
+        onClick: () => {},
+        status: name.trim().length !== 0 ? "active" : "inactive",
+      }}
+      link={{
+        value: t("CHANGE_NAME__CANCEL_BUTTON"),
+        onClick: router.back,
+      }}
+    >
+      <Filed
+        cn="mb-[16px]"
+        placeholder={t("CHANGE_NAME__FILED_PLACEHOLDER")}
+        onChange={setName}
+        value={name}
+      />
+    </PageStructure>
   );
 };
 
