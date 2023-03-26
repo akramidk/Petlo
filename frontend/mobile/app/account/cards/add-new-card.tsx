851e6e742ef1888f56a50a5e7afe836c79225aca
2kram <al.awad7074@gmail.com>
2023-03-26 11:50:39 +0300

    okay

--- frontend/mobile/app/account/cards/add-new-card.tsx
+++ frontend/mobile/app/account/cards/add-new-card.tsx
@@ -1,7 +1,41 @@
 import { PageStructure } from "../../../src/components/organisms";
+import {
+  useStripe,
+  CardField,
+  StripeProvider,
+  CardForm,
+} from "@stripe/stripe-react-native";
+import { useEffect, useState } from "react";
 
 const AddNewCard = () => {
-  return <PageStructure title="Add New Card"></PageStructure>;
+  const { createToken } = useStripe();
+  const [token, setToken] = useState<unknown>();
+
+  useEffect(() => {
+    console.log("token", token);
+  }, [token]);
+
+  return (
+    <PageStructure title="Add New Card">
+      <StripeProvider publishableKey="pk_test_51MLCpbBOEiCvkdTpNPVHQTabYpsAlE2aOm3lH4P6CgOhdYWVDBZs7bpDGHR5ewLjia5eii60w78ZkACtudbrWEun00gDTjfPaX">
+        <CardField
+          postalCodeEnabled={false}
+          onCardChange={(cardDetails) => {
+            createToken({
+              type: "Card",
+            }).then((response) => setToken(response?.token?.id));
+          }}
+          cardStyle={{ textColor: "#000" }}
+          style={{
+            height: 60,
+            backgroundColor: "#f6f6f6",
+            borderRadius: 4,
+            padding: 20,
+          }}
+        />
+      </StripeProvider>
+    </PageStructure>
+  );
 };
 
 export default AddNewCard;
