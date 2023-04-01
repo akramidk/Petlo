89b83a6a561490e7b539c92a4623048e87937192
2kram <al.awad7074@gmail.com>
2023-04-01 17:16:06 +0300

    details

--- backend/app/helpers/addresses_helper/index.rb
+++ backend/app/helpers/addresses_helper/index.rb
@@ -5,7 +5,8 @@ module AddressesHelper::Index
       offset = (LIMIT * page) - LIMIT
       addresses = customer.addresses.limit(LIMIT + 1).offset(offset).map{|address| {
         public_id: address.public_id,
-        name: address.name
+        name: address.name,
+        details: address.details
       }}
   
       { has_more: !!addresses[LIMIT], data: addresses[0..LIMIT-1] }
