242dc7394885aa1f6c4bb83553256bd65de7a94a
2kram <al.awad7074@gmail.com>
2023-04-05 01:45:57 +0300

    options values

--- frontend/mobile/app/item/index.tsx
+++ frontend/mobile/app/item/index.tsx
@@ -1,9 +1,10 @@
 import clsx from "clsx";
 import { useRouter, useSearchParams } from "expo-router";
-import { useMemo } from "react";
+import { useMemo, useRef } from "react";
 import { View, Image } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import { BackButton, Text } from "../../src/components/atoms";
+import { BaseButton } from "../../src/components/bases";
 import { PageStructure } from "../../src/components/organisms";
 import { Endpoints } from "../../src/enums";
 import {
@@ -19,6 +20,7 @@ const Item = () => {
   const { publicId } = useSearchParams();
   const { t } = useTranslationsContext();
   const { direction } = useInternationalizationContext();
+  const scrollViewRef = useRef<ScrollView>();
   const { response } = useAPIFetching<void, ItemResponse>({
     endpoint: Endpoints.ITEM,
     SWROptions: {
@@ -30,11 +32,10 @@ const Item = () => {
   });
 
   const { item, options, variants } = useMemo(() => {
-    if (response.isFetching) return;
+    const item = response?.body;
+    const options = response?.body?.options;
+    const variants = response?.body?.variants;
 
-    const item = response.body;
-    const options = response.body.options;
-    const variants = response.body.variants;
     return { item, options, variants };
   }, [response]);
 
@@ -85,9 +86,44 @@ const Item = () => {
           {options.map((option, i) => {
             return (
               <View key={i}>
-                <Text font="bold" cn="text-[14px] text-[#0E333C]">
+                <Text font="bold" cn="text-[14px] text-[#0E333C] mb-[4px]">
                   {option.name}
                 </Text>
+
+                <ScrollView
+                  className={direction === "ltr" ? "self-start" : "self-end"}
+                  ref={scrollViewRef}
+                  contentContainerStyle={{
+                    flexDirection: direction === "ltr" ? "row" : "row-reverse",
+                  }}
+                  onContentSizeChange={
+                    direction === "rtl"
+                      ? () =>
+                          scrollViewRef.current?.scrollToEnd({
+                            animated: false,
+                          })
+                      : undefined
+                  }
+                  horizontal
+                >
+                  {option.values.map((value, i) => {
+                    return (
+                      <View key={i}>
+                        <BaseButton
+                          onClick={() => {}}
+                          cn={clsx(
+                            "px-[28px] py-[8px] border-[1.4px] border-[#0E333C] rounded-[4px]",
+                            direction === "ltr" ? "mr-[4px]" : "ml-[4px]"
+                          )}
+                        >
+                          <Text font="semiBold" cn="text-[#0E333C] text-[13px]">
+                            {value}
+                          </Text>
+                        </BaseButton>
+                      </View>
+                    );
+                  })}
+                </ScrollView>
               </View>
             );
           })}
