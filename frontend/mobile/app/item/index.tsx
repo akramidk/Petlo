2d9923226248ae471224db5f2566f22cea1db550
2kram <al.awad7074@gmail.com>
2023-04-05 17:11:36 +0300

    Add To Cart

--- frontend/mobile/app/item/index.tsx
+++ frontend/mobile/app/item/index.tsx
@@ -1,9 +1,10 @@
 import clsx from "clsx";
 import { useRouter, useSearchParams } from "expo-router";
-import { useEffect, useMemo, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { View, Image } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
-import { BackButton, Text } from "../../src/components/atoms";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { BackButton, Button, Text } from "../../src/components/atoms";
 import { BaseButton } from "../../src/components/bases";
 import { PageStructure } from "../../src/components/organisms";
 import { Endpoints } from "../../src/enums";
@@ -76,7 +77,7 @@ const Item = () => {
   }
 
   return (
-    <View className="grow">
+    <View className="h-full flex flex-col">
       <View className="h-[306px] p-[56px] bg-[#f6f6f6]">
         <BackButton
           onClick={router.back}
@@ -97,78 +98,96 @@ const Item = () => {
         />
       </View>
 
-      <ScrollView className="p-[28px]">
-        <View className="space-y-[12px] mb-[16]">
-          <View className="space-y-[6px]">
-            <Text font="extraBold" cn="text-[20px] text-[#0E333C]">
-              {item.name}
-            </Text>
+      <ScrollView>
+        <View className="p-[28px]">
+          <View className="space-y-[12px] mb-[16]">
+            <View className="space-y-[6px]">
+              <Text font="extraBold" cn="text-[20px] text-[#0E333C]">
+                {item.name}
+              </Text>
 
-            <Text font="semiBold" cn="text-[14px] text-[#888]">
-              {t("ITEM__BY")} {item.brand}
+              <Text font="semiBold" cn="text-[14px] text-[#888]">
+                {t("ITEM__BY")} {item.brand}
+              </Text>
+            </View>
+
+            <Text font="extraBold" cn="text-[18px] text-[#0E333C]">
+              {variant?.price} {item.currency}
             </Text>
           </View>
 
-          <Text font="extraBold" cn="text-[18px] text-[#0E333C]">
-            {variant?.price} {item.currency}
-          </Text>
-        </View>
+          <View className="space-y-[12px]">
+            {options.map((option, i) => {
+              return (
+                <View key={i}>
+                  <Text font="bold" cn="text-[14px] text-[#0E333C] mb-[4px]">
+                    {option.name}
+                  </Text>
 
-        <View className="space-y-[12px]">
-          {options.map((option, i) => {
-            return (
-              <View key={i}>
-                <Text font="bold" cn="text-[14px] text-[#0E333C] mb-[4px]">
-                  {option.name}
-                </Text>
+                  <ScrollView
+                    className={direction === "ltr" ? "self-start" : "self-end"}
+                    ref={scrollViewRef}
+                    contentContainerStyle={{
+                      flexDirection:
+                        direction === "ltr" ? "row" : "row-reverse",
+                    }}
+                    onContentSizeChange={
+                      direction === "rtl"
+                        ? () =>
+                            scrollViewRef.current?.scrollToEnd({
+                              animated: false,
+                            })
+                        : undefined
+                    }
+                    horizontal
+                  >
+                    {option.values.map((value, y) => {
+                      return (
+                        <View key={y}>
+                          <BaseButton
+                            onClick={() => {
+                              const array = selectedOptions.flat();
+                              array[i] = value;
 
-                <ScrollView
-                  className={direction === "ltr" ? "self-start" : "self-end"}
-                  ref={scrollViewRef}
-                  contentContainerStyle={{
-                    flexDirection: direction === "ltr" ? "row" : "row-reverse",
-                  }}
-                  onContentSizeChange={
-                    direction === "rtl"
-                      ? () =>
-                          scrollViewRef.current?.scrollToEnd({
-                            animated: false,
-                          })
-                      : undefined
-                  }
-                  horizontal
-                >
-                  {option.values.map((value, y) => {
-                    return (
-                      <View key={y}>
-                        <BaseButton
-                          onClick={() => {
-                            const array = selectedOptions.flat();
-                            array[i] = value;
-
-                            setSelectedOptions(array);
-                          }}
-                          cn={clsx(
-                            "px-[28px] py-[8px] border-[1.4px] rounded-[4px]",
-                            direction === "ltr" ? "mr-[4px]" : "ml-[4px]",
-                            value === selectedOptions[i]
-                              ? "border-[#0E333C]"
-                              : "border-[#f6f6f6]"
-                          )}
-                        >
-                          <Text font="semiBold" cn="text-[#0E333C] text-[13px]">
-                            {value} {option.unit}
-                          </Text>
-                        </BaseButton>
-                      </View>
-                    );
-                  })}
-                </ScrollView>
-              </View>
-            );
-          })}
+                              setSelectedOptions(array);
+                            }}
+                            cn={clsx(
+                              "px-[28px] py-[8px] border-[1.4px] rounded-[4px]",
+                              direction === "ltr" ? "mr-[4px]" : "ml-[4px]",
+                              value === selectedOptions[i]
+                                ? "border-[#0E333C]"
+                                : "border-[#f6f6f6]"
+                            )}
+                          >
+                            <Text
+                              font="semiBold"
+                              cn="text-[#0E333C] text-[13px]"
+                            >
+                              {value} {option.unit}
+                            </Text>
+                          </BaseButton>
+                        </View>
+                      );
+                    })}
+                  </ScrollView>
+                </View>
+              );
+            })}
+          </View>
         </View>
       </ScrollView>
+
+      <SafeAreaView
+        className={clsx(
+          "fixed border-t-[1px] border-[#f6f6f6] px-[28px] pt-[16px]"
+        )}
+        edges={["bottom"]}
+      >
+        <Button
+          value="Add To Cart"
+          onClick={() => console.log(variant.public_id)}
+        />
+      </SafeAreaView>
     </View>
   );
 };
