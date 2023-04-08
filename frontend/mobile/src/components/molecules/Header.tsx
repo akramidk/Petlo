70d05a9432c127403b925fa0c01967a0449c0358
2kram <al.awad7074@gmail.com>
2023-04-08 16:50:55 +0300

    fuck 1

--- frontend/mobile/src/components/molecules/Header.tsx
+++ frontend/mobile/src/components/molecules/Header.tsx
@@ -12,7 +12,6 @@ interface HeaderProps {
 
 const Header = ({ activePath, showSearchIcon, showCartIcon }: HeaderProps) => {
   const router = useRouter();
-  const { numberOfItems } = useCartContext();
 
   return (
     <View className="flex-row h-[48px] px-[28px] bg-[#fff] border-b-[1px] border-b-[#f6f6f6] justify-between items-start pt-[6px]">
@@ -29,7 +28,7 @@ const Header = ({ activePath, showSearchIcon, showCartIcon }: HeaderProps) => {
 
             return (
               <View key={i} className="ml-[16px]">
-                {icon.id === "CART" && showCartIcon && numberOfItems > 0 && (
+                {icon.id === "CART" && showCartIcon && (
                   <View className="h-[10px] w-[10px] absolute z-10 rounded-full bg-[#E64848] border-[1px] border-[#fff]"></View>
                 )}
                 <HeaderButton
