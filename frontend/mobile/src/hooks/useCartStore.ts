5d24be071426c974c808c9562cbfa247e384a432
2kram <al.awad7074@gmail.com>
2023-04-08 17:15:19 +0300

    useCartStore

--- frontend/mobile/src/hooks/useCartStore.ts
+++ frontend/mobile/src/hooks/useCartStore.ts
@@ -1,11 +1,15 @@
 import { create } from "zustand";
 
 interface CartStore {
+  cartId: string | undefined | null;
+  setCartId: (cartId: string | null) => void;
   numberofItems: number;
   setNumberofItems: (numberofItems: number) => void;
 }
 
 const useCartStore = create<CartStore>((set) => ({
+  cartId: undefined,
+  setCartId: (cartId) => set(() => ({ cartId: cartId })),
   numberofItems: 0,
   setNumberofItems: (numberofItems) =>
     set(() => ({ numberofItems: numberofItems })),
