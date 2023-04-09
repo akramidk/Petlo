1404be0c971977ec597d2f00fc9710c07ff4855f
2kram <al.awad7074@gmail.com>
2023-04-09 20:27:52 +0300

    ar

--- frontend/mobile/app/(auth)/welcome.tsx
+++ frontend/mobile/app/(auth)/welcome.tsx
@@ -1,10 +1,17 @@
+import clsx from "clsx";
 import { useRouter } from "expo-router";
 import { View, Image } from "react-native";
 import { Button, Logo, Text } from "../../src/components/atoms";
 import { BaseButton } from "../../src/components/bases";
+import {
+  useInternationalizationContext,
+  useTranslationsContext,
+} from "../../src/hooks";
 
 const Welcome = () => {
   const router = useRouter();
+  const { t } = useTranslationsContext();
+  const { direction } = useInternationalizationContext();
 
   return (
     <View
@@ -22,14 +29,19 @@ const Welcome = () => {
           source={require("../../src/assets/images/bowl.webp")}
         />
 
-        <Text font="light" cn="text-[48px] text-[#0E333C]">
-          Welcome to
+        <Text font="light" cn="text-[48px] text-[#0E333C] leading-[80px]">
+          {t("WELCOME__TITLE")}
         </Text>
 
         <Logo cn="text-[52px] text-[#0E333C]" />
       </View>
 
-      <View className="absolute bottom-0 mb-[52px] px-[36px] flex-row space-x-[6px]">
+      <View
+        className={clsx(
+          "absolute bottom-0 mb-[52px] px-[36px] space-x-[6px]",
+          direction === "ltr" ? "flex-row" : "flex-row-reverse"
+        )}
+      >
         <BaseButton
           onClick={() => {
             router.push("/sign-up");
@@ -37,7 +49,7 @@ const Welcome = () => {
           cn="grow h-[52px] bg-[#0E333C] rounded-[4px] items-center justify-center"
         >
           <Text font="semiBold" cn="text-[#eee] text-[14px]">
-            Sign Up
+            {t("WELCOME__SIGN_UP_BUTTON")}
           </Text>
         </BaseButton>
 
@@ -48,7 +60,7 @@ const Welcome = () => {
           cn="grow h-[52px] border-[1px] border-[#0E333C] rounded-[4px] items-center justify-center"
         >
           <Text font="semiBold" cn="text-[#0E333C] text-[14px]">
-            Sign In
+            {t("WELCOME__SIGN_IN_BUTTON")}
           </Text>
         </BaseButton>
       </View>
