525ed4b8da8c0050a8c91efc0a357506983cafb9
2kram <al.awad7074@gmail.com>
2023-04-11 20:54:39 +0300

    sidekiq auth

--- backend/config/initializers/sidekiq.rb
+++ backend/config/initializers/sidekiq.rb
@@ -1,3 +1,5 @@
+require 'sidekiq/web'
+
 Sidekiq.configure_server do |config|
     config.redis = { url: 'redis://redis:6379/1' }
 end
@@ -5,3 +7,7 @@ end
 Sidekiq.configure_client do |config|
     config.redis = { url: 'redis://redis:6379/1' }
 end  
+
+Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    username == ENV["SIDEKIQ_USERNAME"] && password == ENV["SIDEKIQ_PASSWORD"]
+end
