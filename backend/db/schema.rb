efa0ac6a32fffc8d33e87862d127e633c407f66d
2kram <al.awad7074@gmail.com>
2023-04-16 08:05:38 +0300

    mmmm

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_12_125933) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_15_201936) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.bigint "record_id", null: false
@@ -42,6 +42,23 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_12_125933) do
     t.string "latitude", null: false
     t.string "longitude", null: false
     t.string "details"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "banner_details", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "banner_id", null: false
+    t.string "country", null: false
+    t.string "language", null: false
+    t.string "variant", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "banners", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "public_id", null: false
+    t.integer "order"
+    t.string "path"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
