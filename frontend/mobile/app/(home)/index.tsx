b83a25fa17daa782c553438ed0e3567f875375f9
2kram <al.awad7074@gmail.com>
2023-04-16 21:51:33 +0300

    no need for this

--- frontend/mobile/app/(home)/index.tsx
+++ frontend/mobile/app/(home)/index.tsx
@@ -55,9 +55,6 @@ const Home = () => {
     }
   );
 
-  console.log("bannersResponse", bannersResponse);
-  console.log("sectionsResponse", sectionsResponse);
-
   if (
     !storedLanguage ||
     !customer ||
