016badd63c5ea404d06e5fa7e545d9b5845cfb9f
2kram <al.awad7074@gmail.com>
2023-04-30 20:11:31 +0300

    Payment model

--- backend/app/models/payment.rb
+++ backend/app/models/payment.rb
@@ -1,2 +1,16 @@
 class Payment < ApplicationRecord
+    belongs_to :order
+
+    enum status: {
+        uncollected: 0,
+        collected: 1,
+    }
+
+    enum method: {
+        cash: 0,
+        card: 1,
+    }
+
+    validates :public_id, presence: { message:  }, uniqueness: { message:  }
+    validates :order_id, presence: { message:  }, uniqueness: { message:  }
 end
