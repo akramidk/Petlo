fb143b050a085e12b546332ed236f2d24d8b1efb
2kram <al.awad7074@gmail.com>
2023-04-30 20:14:36 +0300

    CardPayment model

--- backend/app/models/card_payment.rb
+++ backend/app/models/card_payment.rb
@@ -1,2 +1,10 @@
 class CardPayment < ApplicationRecord
+    belongs_to :payment
+
+    has_one :card
+
+    validates :payment_id, presence: { message:  }, uniqueness: { message:  }
+    validates :card_id, presence: { message:  }
+    validates :processed_by, presence: { message:  }
+    validates :processor_payment_id, presence: { message:  }
 end
