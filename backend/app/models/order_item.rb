639646c9cf3d317a808da91bc9722058edda46f9
2kram <al.awad7074@gmail.com>
2023-04-30 19:59:28 +0300

    OrderItem model

--- backend/app/models/order_item.rb
+++ backend/app/models/order_item.rb
@@ -1,2 +1,13 @@
 class OrderItem < ApplicationRecord
+    belongs_to :order
+
+    has_one :item
+    has_one :variant
+
+    validates :order_id, presence: { message:  }
+    validates :item_id, presence: { message:  }, uniqueness: { scope: [:order_id], message:  }
+    validates :variant_id, presence: { message:  }, uniqueness: { scope: [:item_id], message:  }
+    validates :price, presence: { message:  }
+    validates :quantity, presence: { message:  }
+    validates :total_price, presence: { message:  }
 end
