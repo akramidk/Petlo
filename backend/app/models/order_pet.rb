98d0ab30f44598888adbc91f5493eeac0bc95d7e
2kram <al.awad7074@gmail.com>
2023-04-30 20:09:11 +0300

    OrderPet model

--- backend/app/models/order_pet.rb
+++ backend/app/models/order_pet.rb
@@ -1,2 +1,8 @@
 class OrderPet < ApplicationRecord
+    belongs_to :order
+
+    has_one :pet
+
+    validates :order_id, presence: { message:  }
+    validates :pet_id, presence: { message:  }, uniqueness: { scope: [:order_id], message:  }
 end
