6353b6bcae307ca20ec50aeec49fd3ec1dbe54be
2kram <al.awad7074@gmail.com>
2023-04-30 16:00:58 +0300

    fix createing a checkout

--- backend/app/helpers/checkout_helper/create.rb
+++ backend/app/helpers/checkout_helper/create.rb
@@ -5,17 +5,22 @@ module CheckoutHelper::Create
         raise(RuntimeError, 3005001) if cart.used?
         raise(RuntimeError, 3005002) if cart.created_at + CONSTANTS::TIMES[:CART_EXP_AFTER] < Time.now
 
-        address = customer.addresses.find_by(public_id: address_id)
-        raise(RuntimeError, 3005003) if !address
+        address = nil
+        delivery_amount = nil
+        if(address_id)
+            address = customer.addresses.find_by(public_id: address_id)
+            raise(RuntimeError, 3005003) if !address
+            
+            delivery_amount = 2 // TODO
+        end
 
         cart_amount = cart.total(country: customer.country)
-        delivery_amount = 2000 #not ready yet
         currency = CONSTANTS::COUNTRIES_CURRENCIES[customer.country]["en"]
 
         checkout = Checkout.create!(
             customer_id: customer.id,
             cart_id: cart.id,
-            address_id: address.id,
+            address_id: address&.id,
             cart_amount: cart_amount,
             delivery_amount: delivery_amount,
             currency: currency
