a2b261fc6192cae6a70e1303000d65a5f1ee7b54
2kram <al.awad7074@gmail.com>
2023-05-03 02:07:04 +0300

    errors

--- backend/app/helpers/orders_helper/create.rb
+++ backend/app/helpers/orders_helper/create.rb
@@ -3,14 +3,14 @@ module OrdersHelper::Create
 
     def create(customer:, checkout_id:, payment:, pets:)
         checkout = customer.checkouts.find_by(public_id: checkout_id)
-        raise(RuntimeError, ) if !checkout
-        raise(RuntimeError, ) if checkout.used?
-        raise(RuntimeError, ) if checkout.created_at + CONSTANTS::TIMES[:CHECKOUT_EXP_AFTER] < Time.now
+        raise(RuntimeError, 3006000) if !checkout
+        raise(RuntimeError, 3006001) if checkout.used?
+        raise(RuntimeError, 3006002) if checkout.created_at + CONSTANTS::TIMES[:CHECKOUT_EXP_AFTER] < Time.now
 
         cart = customer.carts.find_by(id: checkout.cart_id)
-        raise(RuntimeError, ) if !cart
-        raise(RuntimeError, ) if cart.used?
-        raise(RuntimeError, ) if cart.created_at + CONSTANTS::TIMES[:CART_EXP_AFTER] < Time.now
+        raise(RuntimeError, 3006003) if !cart
+        raise(RuntimeError, 3006004) if cart.used?
+        raise(RuntimeError, 3006005) if cart.created_at + CONSTANTS::TIMES[:CART_EXP_AFTER] < Time.now
 
         if payment[:type] === "card"
             begin
@@ -23,7 +23,7 @@ module OrdersHelper::Create
                     }
                 )
               rescue
-                raise(RuntimeError, )
+                raise(RuntimeError, 3006006)
             end
         end
 
