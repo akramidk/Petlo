a57b0852e5fb6aec53d36519ea45e5e635f79fff
2kram <al.awad7074@gmail.com>
2023-05-05 20:18:07 +0300

    card

--- frontend/mobile/app/checkout/index.tsx
+++ frontend/mobile/app/checkout/index.tsx
@@ -1,11 +1,15 @@
 import { useRouter, useSearchParams } from "expo-router";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { View, Text } from "react-native";
 import { PageStructure } from "../../src/components/organisms";
 import { PAYMENT_METHODS } from "../../src/constants";
 import { Endpoints } from "../../src/enums";
-import { useAPIMutation } from "../../src/hooks";
-import { BaseOption } from "../../src/interfaces";
+import { useAPIFetching, useAPIMutation } from "../../src/hooks";
+import {
+  BaseOption,
+  CustomerCardsRequest,
+  CustomerCardsResponse,
+} from "../../src/interfaces";
 import Loading from "../_Loading";
 import { Options, OptionsWithLabel } from "../../src/components/atoms";
 
@@ -23,6 +27,7 @@ const Checkout = () => {
   });
 
   const [paymentMethod, setPaymentMethod] = useState<BaseOption>();
+  const [card, setCard] = useState<BaseOption>();
 
   useEffect(() => {
     createCheckoutTrigger({
@@ -30,9 +35,30 @@ const Checkout = () => {
     });
   }, []);
 
+  const { response: cardsResponse } = useAPIFetching<
+    CustomerCardsRequest,
+    CustomerCardsResponse
+  >({
+    endpoint: Endpoints.CUSTOMER_CARDS,
+    options: {
+      withPagination: true,
+    },
+  });
+
+  const cards = useMemo(() => {
+    return cardsResponse.body.data?.map((card) => {
+      return {
+        id: card.public_id,
+        value: card.last4,
+      };
+    });
+  }, []);
+
   if (
     createCheckoutResponse === undefined ||
-    createCheckoutResponse?.status === "loading"
+    createCheckoutResponse?.status === "loading" ||
+    cardsResponse === undefined ||
+    cardsResponse.isFetching
   ) {
     return <Loading />;
   }
@@ -58,22 +84,25 @@ const Checkout = () => {
           }}
         />
 
-        <OptionsWithLabel
-          label={{
-            name: "Payment Method",
-            require: true,
-          }}
-          options={{
-            optionValueCn: "text-[#666]",
-            optionValueFont: "semiBold",
-            options: PAYMENT_METHODS,
-            signalSelect: {
-              selectedOption: paymentMethod,
-              setSelectedOption: setPaymentMethod,
-            },
-            translate: true,
-          }}
-        />
+        {paymentMethod?.id === "card" && (
+          <View>
+            <OptionsWithLabel
+              label={{
+                name: "Select a Card",
+                require: true,
+              }}
+              options={{
+                optionValueCn: "text-[#666]",
+                optionValueFont: "semiBold",
+                options: cards,
+                signalSelect: {
+                  selectedOption: card,
+                  setSelectedOption: setCard,
+                },
+              }}
+            />
+          </View>
+        )}
       </View>
     </PageStructure>
   );
