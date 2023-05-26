561f87874686595dc0ae0f04e3a261eda95a0a2d
2kram <al.awad7074@gmail.com>
2023-05-26 17:39:29 +0300

    AutoshipPet model

--- backend/app/models/autoship_pet.rb
+++ backend/app/models/autoship_pet.rb
@@ -1,2 +1,6 @@
 class AutoshipPet < ApplicationRecord
+  belongs_to :autoship
+
+  validates :autoship_id, presence: { message: 2015000 }
+  validates :pet_id, presence: { message: 2015001 }, uniqueness: { scope: :autoship_id, message: 2015002 }
 end
