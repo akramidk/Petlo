057de3494e7fde3a3ca4f67bca4d50bde56f3d3b
2kram <al.awad7074@gmail.com>
2023-05-26 13:34:47 +0300

    autoship items errors code

--- backend/app/models/autoship_item.rb
+++ backend/app/models/autoship_item.rb
@@ -1,7 +1,7 @@
 class AutoshipItem < ApplicationRecord
   belongs_to :autoship
 
-  validates :autoship_id, presence: { message: 1 }
-  validates :item_id, presence: { message: 1 }
-  validates :variant_id, presence: { message: 1 }
+  validates :autoship_id, presence: { message: 2014000 }
+  validates :item_id, presence: { message: 2014001 }
+  validates :variant_id, presence: { message: 2014002 }
 end
