b5565f2c852c3463c677794db924c809e7b889a9
2kram <al.awad7074@gmail.com>
2023-05-26 14:37:36 +0300

    AutoshipsController create

--- backend/app/controllers/v1/autoships_controller.rb
+++ backend/app/controllers/v1/autoships_controller.rb
@@ -1,6 +1,21 @@
 module V1
   class AutoshipsController < ApplicationController
+    before_action -> { current_customer(verified: true) }
+
+    include AutoshipsHelper
+
     def create
+      AutoshipsHelper.create(
+        customer: @customer,
+        name: params[:name],
+        recurring_interval: params[:recurring_interval],
+        recurring_interval_count: params[:recurring_interval_count],
+        starting_from: params[:starting_from],
+        items: params[:items],
+        payment: params[:payment],
+        pets: params[:pets]
+      )
+
       render json: { status: "succeeded" }, status: 200
     end
   end
