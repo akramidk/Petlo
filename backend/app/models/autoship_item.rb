113f61a762bb484508225e7f33dd3225d8c5ea84
2kram <al.awad7074@gmail.com>
2023-05-26 18:32:06 +0300

    changes on AutoshipItem model

--- backend/app/models/autoship_item.rb
+++ backend/app/models/autoship_item.rb
@@ -4,4 +4,5 @@ class AutoshipItem < ApplicationRecord
   validates :autoship_id, presence: { message: 2014000 }
   validates :item_id, presence: { message: 2014001 }
   validates :variant_id, presence: { message: 2014002 }
+  validates :quantity, presence: { message: 2015003 }, numericality: { greater_than: 0, message: 2015004 }
 end
