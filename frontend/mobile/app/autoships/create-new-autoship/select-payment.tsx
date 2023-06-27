6ff1834d9386c1778d8560bdff777d0434bdba9a
2kram <al.awad7074@gmail.com>
2023-06-27 16:48:50 +0300

    SelectPayment 2

--- frontend/mobile/app/autoships/create-new-autoship/select-payment.tsx
+++ frontend/mobile/app/autoships/create-new-autoship/select-payment.tsx
@@ -1,15 +1,65 @@
 import { useRouter } from "expo-router";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { PageStructure } from "../../../src/components/organisms";
-import { useDataContext, useTranslationsContext } from "../../../src/hooks";
-import { BaseOption } from "../../../src/interfaces";
+import { PAYMENT_METHODS } from "../../../src/constants";
+import {
+  useAPIFetching,
+  useDataContext,
+  useInternationalizationContext,
+  useTranslationsContext,
+} from "../../../src/hooks";
+import {
+  BaseOption,
+  CustomerCardsRequest,
+  CustomerCardsResponse,
+} from "../../../src/interfaces";
+import { Link, OptionsWithLabel } from "../../../src/components/atoms";
+import { View } from "react-native";
+import { Endpoints } from "../../../src/enums";
+import { cardToDataCard } from "../../../src/utils";
+import { DataCard } from "../../../src/components/molecules";
+import Loading from "../../_Loading";
 
 const SelectPayment = () => {
   const router = useRouter();
   const { t } = useTranslationsContext();
   const { data, setData } = useDataContext();
+  const { direction } = useInternationalizationContext();
 
-  const [payment, setPayment] = useState<BaseOption>();
+  const [paymentMethod, setPaymentMethod] = useState<BaseOption>();
+  const [card, setCard] = useState<BaseOption>();
+
+  const { response: cardsResponse } = useAPIFetching<
+    CustomerCardsRequest,
+    CustomerCardsResponse
+  >({
+    endpoint: Endpoints.CUSTOMER_CARDS,
+    options: {
+      withPagination: true,
+    },
+  });
+
+  const cards = useMemo(() => {
+    return cardsResponse.body?.data?.map((card) => {
+      return {
+        id: card.public_id,
+        value: (
+          <DataCard
+            {...cardToDataCard({
+              card: card,
+              direction: direction,
+              t: t,
+            })}
+            withoutContainerStyles
+          />
+        ) as React.ReactNode,
+      };
+    });
+  }, [cardsResponse]);
+
+  if (cardsResponse === undefined || cardsResponse.isFetching) {
+    return <Loading />;
+  }
 
   return (
     <PageStructure
@@ -23,13 +73,50 @@ const SelectPayment = () => {
 
           router.back();
         },
-        status:
-          payment === undefined || payment?.id === data?.payment?.public_id
-            ? "inactive"
-            : "active",
       }}
       link={{ value: "Cancel", onClick: router.back }}
-    ></PageStructure>
+    >
+      <OptionsWithLabel
+        cn="mb-[24px]"
+        options={{
+          optionValueCn: "text-[#666]",
+          optionValueFont: "semiBold",
+          options: PAYMENT_METHODS,
+          signalSelect: {
+            selectedOption: paymentMethod,
+            setSelectedOption: setPaymentMethod,
+          },
+          translate: true,
+        }}
+      />
+
+      {paymentMethod?.id === "card" && (
+        <View className="mb-[40px]">
+          <OptionsWithLabel
+            cn="mb-[12px]"
+            label={{
+              name: t("CHECKOUT__SELECT_A_CARD"),
+              require: true,
+            }}
+            options={{
+              optionValueCn: "text-[#666]",
+              optionValueFont: "semiBold",
+              options: cards,
+              signalSelect: {
+                selectedOption: card,
+                setSelectedOption: setCard,
+              },
+            }}
+          />
+
+          <Link
+            valueCN="text-[#9747FF] text-[14px]"
+            value="+ Add New Card To Use"
+            onClick={() => router.push("/account/cards/add-new-card")}
+          />
+        </View>
+      )}
+    </PageStructure>
   );
 };
 
