3a55f954c2ff3f52302a0cd8e116dc652ef10a58
2kram <al.awad7074@gmail.com>
2023-07-01 21:07:52 +0300

    bla

--- frontend/mobile/app/autoships/create-new-autoship/select-date.tsx
+++ frontend/mobile/app/autoships/create-new-autoship/select-date.tsx
@@ -1,5 +1,8 @@
 import { PageStructure } from "../../../src/components/organisms";
-import { useTranslationsContext } from "../../../src/hooks";
+import {
+  useInternationalizationContext,
+  useTranslationsContext,
+} from "../../../src/hooks";
 import { useRouter } from "expo-router";
 import { useState } from "react";
 import SelectNextShipment from "./components/SelectNextShipment";
@@ -8,10 +11,13 @@ import SelectThePeriod from "./components/SelectThePeriod";
 import { View } from "react-native";
 import { AUTOSHIP_RECURRING_INTERVAL_OPTIONS } from "../../../src/constants";
 import { BaseOption } from "../../../src/interfaces";
+import { Text } from "../../../src/components/atoms";
+import clsx from "clsx";
 
 const SelectDate = () => {
   const router = useRouter();
   const { t } = useTranslationsContext();
+  const { direction } = useInternationalizationContext();
 
   const [nextShipment, setNextShipment] = useState<NextShipment>();
   const [recurringIntervalCount, setRecurringIntervalCount] =
@@ -19,6 +25,17 @@ const SelectDate = () => {
   const [recurringInterval, setRecurringInterval] = useState<BaseOption>(
     AUTOSHIP_RECURRING_INTERVAL_OPTIONS[0]
   );
+
+  const recurringIntervalCountAsNumber = Number(recurringIntervalCount);
+
+  const isValid =
+    nextShipment &&
+    ((recurringInterval.id === "day" &&
+      recurringIntervalCountAsNumber >= 5 &&
+      recurringIntervalCountAsNumber <= 90) ||
+      (recurringInterval.id === "month" &&
+        recurringIntervalCountAsNumber >= 1 &&
+        recurringIntervalCountAsNumber <= 3));
 
   return (
     <PageStructure
@@ -40,6 +57,40 @@ const SelectDate = () => {
         recurringInterval={recurringInterval}
         setRecurringInterval={setRecurringInterval}
       />
+
+      {isValid && (
+        <View className="space-y-[12px] mt-[32px]">
+          <View
+            className={clsx(
+              "justify-between",
+              direction === "ltr" ? "flex-row" : "flex-row-reverse"
+            )}
+          >
+            <Text font="semiBold" cn="text-[14px] text-[#666]">
+              First shipment on
+            </Text>
+            <Text font="semiBold" cn="text-[14px] text-[#666]">
+              {nextShipment
+                ? `${nextShipment.day}/${nextShipment.month}/${nextShipment.year}`
+                : "not selected"}
+            </Text>
+          </View>
+
+          <View
+            className={clsx(
+              "justify-between",
+              direction === "ltr" ? "flex-row" : "flex-row-reverse"
+            )}
+          >
+            <Text font="semiBold" cn="text-[14px] text-[#666]">
+              Then every
+            </Text>
+            <Text font="semiBold" cn="text-[14px] text-[#666]">
+              {recurringIntervalCountAsNumber} {t(recurringInterval.value)}
+            </Text>
+          </View>
+        </View>
+      )}
     </PageStructure>
   );
 };
