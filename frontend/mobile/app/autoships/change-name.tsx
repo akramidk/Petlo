8c0dd1d1be146a255cbe74d181f5806df53efb3e
2kram <al.awad7074@gmail.com>
2023-07-09 16:07:31 +0300

    ChangeName 1

--- frontend/mobile/app/autoships/change-name.tsx
+++ frontend/mobile/app/autoships/change-name.tsx
@@ -1,11 +1,36 @@
+import { useRouter } from "expo-router";
 import { View } from "react-native";
 import { Text } from "../../src/components/atoms";
+import { PageStructure } from "../../src/components/organisms";
+import { useTranslationsContext } from "../../src/hooks";
+import { Filed } from "../../src/components/atoms";
+import { useState } from "react";
 
 const ChangeName = () => {
+  const router = useRouter();
+  const { t } = useTranslationsContext();
+
+  const [name, setName] = useState("");
+
   return (
-    <View>
-      <Text>bla</Text>
-    </View>
+    <PageStructure
+      title="Change Autoship Name"
+      button={{
+        value: t("COMMON__CHANGE"),
+        onClick: () => {},
+        status: name.trim().length > 0 ? "active" : "inactive",
+      }}
+      link={{
+        value: t("COMMON__CANCEL"),
+        onClick: router.back,
+      }}
+    >
+      <Filed
+        placeholder={t("CHANGE_NAME__FILED_PLACEHOLDER")}
+        onChange={setName}
+        value={name}
+      />
+    </PageStructure>
   );
 };
 
