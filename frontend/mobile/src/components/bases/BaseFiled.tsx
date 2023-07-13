491adc9b8fb76c9288afa042840a0abffea14a45
2kram <al.awad7074@gmail.com>
2023-07-13 20:42:58 +0300

    convert ar numbers to en numbers

--- frontend/mobile/src/components/bases/BaseFiled.tsx
+++ frontend/mobile/src/components/bases/BaseFiled.tsx
@@ -2,6 +2,33 @@ import { TextInput } from "react-native";
 import clsx from "clsx";
 import { useInternationalizationContext } from "../../hooks";
 import { BaseFiledProps } from "../../interfaces";
+import { useCallback } from "react";
+//
+const NUMBERS = {
+  // ar
+  "٠": "0",
+  "١": "1",
+  "٢": "2",
+  "٣": "3",
+  "٤": "4",
+  "٥": "5",
+  "٦": "6",
+  "٧": "7",
+  "٨": "8",
+  "٩": "9",
+
+  //en
+  "0": "0",
+  "1": "1",
+  "2": "2",
+  "3": "3",
+  "4": "4",
+  "5": "5",
+  "6": "6",
+  "7": "7",
+  "8": "8",
+  "9": "9",
+};
 
 const BaseFiled = ({
   value,
@@ -14,6 +41,23 @@ const BaseFiled = ({
 }: BaseFiledProps) => {
   const { languageWithoutGender, direction } = useInternationalizationContext();
 
+  const onChangeHandler = useCallback(
+    (text: string) => {
+      if (keyboardType !== "number-pad") {
+        onChange(text);
+        return;
+      }
+
+      onChange(
+        text
+          .split("")
+          .map((number) => NUMBERS[number])
+          .join("")
+      );
+    },
+    [onChange, keyboardType]
+  );
+
   return (
     <TextInput
       className={clsx(
@@ -23,7 +67,7 @@ const BaseFiled = ({
         cn
       )}
       value={value}
-      onChangeText={onChange}
+      onChangeText={onChangeHandler}
       placeholder={placeholder}
       placeholderTextColor="#aaa"
       keyboardType={keyboardType}
