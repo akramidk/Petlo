30d220b7a73c020c861a110fb91442e39b0d3eb9
2kram <al.awad7074@gmail.com>
2023-07-13 17:49:59 +0300

    fixed invalid pet breed

--- backend/app/models/pet.rb
+++ backend/app/models/pet.rb
@@ -9,8 +9,8 @@ class Pet < ApplicationRecord
   validates :customer_id, presence: { message: 2001002 }
   validates :name, presence: { message: 2001003 }, uniqueness: { scope: [:kind, :breed, :gender], message: 2001004 }
   validates :kind, presence: { message: 2001005 }, inclusion: { in: CONSTANTS::PETS.keys, message: 2001006 }
-  validates :breed, presence: { message: 2001007 }, inclusion: { in: CONSTANTS::PETS["dog"][:breeds].keys, message: 2001008 }, if: -> { kind == "dog" }
   #TODO should be without transform_keys
+  validates :breed, presence: { message: 2001007 }, inclusion: { in: CONSTANTS::PETS["dog"][:breeds].transform_keys(&:to_s).keys, message: 2001008 }, if: -> { kind == "dog" }
   validates :breed, presence: { message: 2001007 }, inclusion: { in: CONSTANTS::PETS["cat"][:breeds].transform_keys(&:to_s).keys, message: 2001008 }, if: -> { kind == "cat" }
   validates :gender, presence: { message: 2001009 }, inclusion: { in: CONSTANTS::PET_GENDERS.keys, message: 2001010 }
 end
