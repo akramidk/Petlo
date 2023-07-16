527f209d1e6a497de69f28552f3ed4d85ae689aa
2kram <al.awad7074@gmail.com>
2023-07-16 11:39:55 +0300

    update_date

--- backend/app/controllers/v1/autoships_controller.rb
+++ backend/app/controllers/v1/autoships_controller.rb
@@ -139,5 +139,17 @@ module V1
 
       render json: response, status: 200
     end
+
+    def update_date
+      AutoshipsHelper.update_date(
+        customer: @customer,
+        public_id: params[:public_id],
+        recurring_interval: params[:recurring_interval],
+        recurring_interval_count: params[:recurring_interval_count],
+        next_shipment_on: params[:next_shipment_on],
+      )
+
+      render json: { status: "succeeded" }, status: 200
+    end
   end
 end
