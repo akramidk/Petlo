092f9f2060d70546e73a717bcee30ce0c7992fbb
2kram <al.awad7074@gmail.com>
2023-07-29 13:57:28 +0300

    Category bug

--- backend/app/models/category/items.rb
+++ backend/app/models/category/items.rb
@@ -1,6 +1,5 @@
 module Category::Items
   @@category = nil
-  @@subcategory = nil
   @@country = nil
   @@language = nil
   @@with_unavailable_items = nil
@@ -8,13 +7,12 @@ module Category::Items
   @@offset = nil
 
   def items(category:, country:, language:, with_unavailable_items: false, limit: nil, offset: nil)
-    @@category = category
+    @@category = Category.find_by(name: category)
     @@country = country
     @@language = language
     @@with_unavailable_items = with_unavailable_items
     @@limit = limit
     @@offset = offset
-    @@subcategory = get_subcategory
 
     items = {
       has_more: @@limit || @@offset ? retrieve_items(limit: 1, offset: @@offset.to_i + @@limit.to_i).exists? : false,
@@ -26,21 +24,8 @@ module Category::Items
   end
 
   private
-  def get_subcategory
-    subcategory = nil
-
-    @@category.split("-").each do |name|
-      params = subcategory.nil? ? { name: name } : { name: name, parent_id: subcategory.id }
-      subcategory = Category.find_by(params)
-      
-      raise(RuntimeError, 2004000) unless subcategory
-    end
-
-    subcategory
-  end
-
   def retrieve_items(limit: nil, offset: nil)
-    @@subcategory.items.joins(:availabilities).where(
+    @@category.items.joins(:availabilities).where(
       availabilities: {
         country: @@country,
         value: @@with_unavailable_items ? [true, false] : true
