c41d56aef71428369b896c8f398c8dd319668a69
2kram <al.awad7074@gmail.com>
2023-08-05 16:22:37 +0300

    changes on Warning

--- frontend/mobile/app/(home)/_components/Warning.tsx
+++ frontend/mobile/app/(home)/_components/Warning.tsx
@@ -3,26 +3,34 @@ import { Text } from "../../../src/components/atoms";
 import { BaseButton } from "../../../src/components/bases";
 
 interface WarningParams {
-  firstText: string;
+  firstText?: string;
   secondText: string;
-  onClick: () => void;
+  onClick?: () => void;
 }
 
 export const Warning = ({ firstText, secondText, onClick }: WarningParams) => {
+  const Container = ({ children, cn }) =>
+    onClick ? (
+      <BaseButton cn={cn} onClick={onClick}>
+        {children}
+      </BaseButton>
+    ) : (
+      <View className={cn}>{children}</View>
+    );
+
   return (
-    <BaseButton
-      cn="px-[16px] py-[12px] border-[1px] border-[#eee] bg-[#f8f8f8] mx-[28px] rounded-[8px]"
-      onClick={onClick}
-    >
+    <Container cn="px-[16px] py-[12px] border-[1px] border-[#eee] bg-[#f8f8f8] mx-[28px] rounded-[8px]">
       <View className="space-y-[4px]">
-        <Text font="bold" cn="text-[#333]">
-          {firstText}
-        </Text>
+        {firstText && (
+          <Text font="bold" cn="text-[#333]">
+            {firstText}
+          </Text>
+        )}
 
         <Text font="medium" cn="text-[#666]">
           {secondText}
         </Text>
       </View>
-    </BaseButton>
+    </Container>
   );
 };
