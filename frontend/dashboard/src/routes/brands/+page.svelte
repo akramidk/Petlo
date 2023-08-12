ca691042f3adf0e9a2f855da662816eadc38e0bf
Baraa Al-Masri <m.baraa.masri@gmail.com>
2023-08-12 15:21:52 +0300

    added brand add page

---
+++ frontend/dashboard/src/routes/brands/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import RequestOptions from "$lib/components/RequestOptions.svelte"
+
+    let data = {
+        en_name: "",
+        ar_name: "",
+    }
+</script>
+
+<RequestOptions bind:data={data} method="POST" actionName="/brands" />
+<input bind:value={data.en_name} required type="text" placeholder="English name" class="input input-bordered w-full max-w-xs" />
+<input bind:value={data.ar_name} required type="text" placeholder="Arabic name" class="input input-bordered w-full max-w-xs" />
+
+
