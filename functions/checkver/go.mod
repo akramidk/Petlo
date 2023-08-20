40d85bffc5bbd8580da0b48516b9f93d0d2e3f09
Baraa Al-Masri <m.baraa.masri@gmail.com>
2023-08-20 19:34:29 +0300

    added app version checker function thingy

---
+++ functions/checkver/go.mod
@@ -0,0 +1,3 @@
+module checkver
+
+go 1.21.0
---
+++ functions/checkver/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+var appVersions = map[string]string{
+	"ios":     "1.1.0",
+	"android": "1.1.0",
+}
+
+func handleCheckAppVersion(res http.ResponseWriter, req *http.Request) {
+	resp := make(map[string]any)
+
+	phoneOS := req.URL.Query().Get("phone_os")
+	appVersion := req.URL.Query().Get("app_version")
+	switch {
+	case len(phoneOS) == 0 && len(appVersion) == 0:
+		resp["value"] = false
+	default:
+		resp["value"] = appVersion < appVersions[phoneOS]
+	}
+
+	_ = json.NewEncoder(res).Encode(resp)
+}
+
+func main() {
+	http.HandleFunc("/", handleCheckAppVersion)
+	http.ListenAndServe(":80", nil)
+}
