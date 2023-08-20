7d7cc650c7f943ca9ff9d89778b981416c1249f7
2kram <al.awad7074@gmail.com>
2023-08-20 17:22:50 +0300

    added the version

--- frontend/mobile/app/more/index.tsx
+++ frontend/mobile/app/more/index.tsx
@@ -10,47 +10,57 @@ import {
   useTranslationsContext,
 } from "../../src/hooks";
 import Scrollable from "../_Scrollable";
+import * as Application from "expo-application";
 
 const More = () => {
   const router = useRouter();
   const { t } = useTranslationsContext();
   const { direction } = useInternationalizationContext();
+  const version = Application.nativeApplicationVersion;
 
   return (
-    <Scrollable cn="space-y-[28px] bg-[#fff]">
-      {MORE_PAGE_SECTIONS.map((section, i) => {
-        return (
-          <View key={i}>
-            <Text font="extraBold" cn="text-[16px] text-[#0E333C] mb-[4px]">
-              {t(section.title)}
-            </Text>
+    <Scrollable cn="abg-[#fff]">
+      <View className="space-y-[28px]">
+        {MORE_PAGE_SECTIONS.map((section, i) => {
+          return (
+            <View key={i}>
+              <Text font="extraBold" cn="text-[16px] text-[#0E333C] mb-[4px]">
+                {t(section.title)}
+              </Text>
 
-            <View>
-              {section.options.map((option, i) => {
-                return (
-                  <Fragment key={i}>
-                    <BaseButton
-                      onClick={() => router.push(option.path)}
-                      cn="border-b-[1px] border-b-[#f6f6f6] py-[18px] items-center"
-                    >
-                      <Icon name={option.icon} size={18} color="#777" />
-                      <Text
-                        font="medium"
-                        cn={clsx(
-                          "text-[14px] text-[#777]",
-                          direction === "ltr" ? "ml-[10px]" : "mr-[10px]"
-                        )}
+              <View>
+                {section.options.map((option, i) => {
+                  return (
+                    <Fragment key={i}>
+                      <BaseButton
+                        onClick={() => router.push(option.path)}
+                        cn="border-b-[1px] border-b-[#f6f6f6] py-[18px] items-center"
                       >
-                        {t(option.name)}
-                      </Text>
-                    </BaseButton>
-                  </Fragment>
-                );
-              })}
+                        <Icon name={option.icon} size={18} color="#777" />
+                        <Text
+                          font="medium"
+                          cn={clsx(
+                            "text-[14px] text-[#777]",
+                            direction === "ltr" ? "ml-[10px]" : "mr-[10px]"
+                          )}
+                        >
+                          {t(option.name)}
+                        </Text>
+                      </BaseButton>
+                    </Fragment>
+                  );
+                })}
+              </View>
             </View>
-          </View>
-        );
-      })}
+          );
+        })}
+      </View>
+
+      <View className="mt-[22px] items-center">
+        <Text font="semiBold" className="text-[#888]">
+          {t("COMMON__APP_VERSION", { version: version })}
+        </Text>
+      </View>
     </Scrollable>
   );
 };
