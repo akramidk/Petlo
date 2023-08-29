bd174c21e7209f4b93929be5b7b4e1fa6450d62f
2kram <al.awad7074@gmail.com>
2023-08-29 17:30:02 +0300

    ar

--- frontend/mobile/app/autoships/components/AboutAutoship.tsx
+++ frontend/mobile/app/autoships/components/AboutAutoship.tsx
@@ -1,32 +1,42 @@
+import clsx from "clsx";
 import { View, Text as ReactText } from "react-native";
 import { Icon, Text } from "../../../src/components/atoms";
-import { useTranslationsContext } from "../../../src/hooks";
+import {
+  useInternationalizationContext,
+  useTranslationsContext,
+} from "../../../src/hooks";
 
 export const AboutAutoship = () => {
   const { t } = useTranslationsContext();
+  const { direction } = useInternationalizationContext();
 
   const points: string[] = Object.keys(t("ABOUT_AUTOSHIP__POINTS")).map(
     (key) => t("ABOUT_AUTOSHIP__POINTS")[key]
   );
 
   return (
-    <View>
+    <View className="space-y-[4px]">
       <View className="bg-[#E7E3D8] rounded-[4px] p-[32]">
-        <View className="space-y-[32px]">
-          <View className="space-y-[8px]">
-            <ReactText className="font-chillax-bold text-[#0E333C] text-[20px]">
+        <View className="space-y-[20px]">
+          <View className={"space-y-[8px]"}>
+            <ReactText
+              className={clsx(
+                "font-chillax-bold text-[#0E333C] text-[20px] flex",
+                direction === "ltr" ? "text-left" : "text-right"
+              )}
+            >
               Autoship
             </ReactText>
 
-            <View className="space-y-[6px]">
-              <Text font="extraBold" cn="text-[24px] text-[#363636] leading-9">
-                {t("ABOUT_AUTOSHIP__TITLE")}
-              </Text>
-
-              <Text font="semiBold" cn="text-[16px] text-[#555]">
-                No More Going To Stores.
-              </Text>
-            </View>
+            <Text
+              font="extraBold"
+              cn={clsx(
+                "text-[24px] text-[#363636] ",
+                direction === "ltr" ? "leading-9" : "leading-[44px]"
+              )}
+            >
+              {t("ABOUT_AUTOSHIP__TITLE")}
+            </Text>
           </View>
 
           <View className="space-y-[12px]">
@@ -34,10 +44,19 @@ export const AboutAutoship = () => {
               return (
                 <View
                   key={index}
-                  className="flex flex-row items-center space-x-[6px]"
+                  className={clsx(
+                    "flex items-center",
+                    direction === "ltr" ? "flex-row" : "flex-row-reverse"
+                  )}
                 >
                   <Icon name="checkCircle" size={22} color="#4A956D" />
-                  <Text font="semiBold" cn="text-[#555] text-[15px]">
+                  <Text
+                    font="semiBold"
+                    cn={clsx(
+                      "text-[#555] text-[15px]",
+                      direction === "ltr" ? "ml-[6px]" : "mr-[6px]"
+                    )}
+                  >
                     {point}
                   </Text>
                 </View>
@@ -45,6 +64,12 @@ export const AboutAutoship = () => {
             })}
           </View>
         </View>
+      </View>
+
+      <View className="bg-[#E7E3D8] rounded-[4px] p-[32]">
+        <Text font="extraBold" cn="text-[#363636] text-[16px]">
+          Common Questions
+        </Text>
       </View>
     </View>
   );
