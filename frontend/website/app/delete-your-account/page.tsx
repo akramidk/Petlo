cdeb70e3b58ccd590830b16488ec54ced4160961
2kram <al.awad7074@gmail.com>
2023-09-20 21:17:13 +0300

    DeleteYourAccount I think the last one

--- frontend/website/app/delete-your-account/page.tsx
+++ frontend/website/app/delete-your-account/page.tsx
@@ -25,6 +25,9 @@ const DeleteYourAccount = () => {
 
   const [phoneNumber, setPhoneNumber] = useState("");
   const [password, setPassword] = useState("");
+  const [verificationCode, setVerificationCode] = useState("");
+  const [token, setToken] = useState("");
+
   const [isLoading, setIsLoading] = useState(false);
   const { enqueueSnackbar } = useSnackbar();
 
@@ -39,7 +42,8 @@ const DeleteYourAccount = () => {
           password: password,
         }
       )
-      .then(() => {
+      .then((response) => {
+        setToken(response.data.customer.session_token);
         setIsLoading(false);
         setStep(2);
       })
@@ -49,8 +53,30 @@ const DeleteYourAccount = () => {
       });
   };
 
+  const secondStepHandler = async () => {
+    setIsLoading(true);
+
+    await axios
+      .delete(`${API_URL}/customers/delete-for-credentials-method`, {
+        headers: { Authorization: `bearer ${token}` },
+        data: {
+          verification_code: Number(verificationCode),
+        },
+      })
+      .then(() => {
+        setIsLoading(false);
+        setStep(3);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        enqueueSnackbar(error.response.data.error.message);
+      });
+  };
+
   const isFirstStepInfoCompleted =
     phoneNumber.trim().length > 0 && password.trim().length > 0;
+
+  const isSecondStepInfoCompleted = verificationCode.trim().length > 0;
 
   if (step === 1) {
     return (
@@ -119,7 +145,63 @@ const DeleteYourAccount = () => {
   }
 
   if (step === 2) {
-    return <></>;
+    return (
+      <div className="bg-[#fff] p-[28px] md:p-[0px] md:h-screen md:flex md:justify-center md:items-center">
+        <div className="md:w-[372px] md:py-[72px]">
+          <div className="mb-[32px]">
+            <div className="font-extrabold text-[24px] text-[#222] mb-[4px]">
+              Confirm Deleting
+            </div>
+
+            <div className="font-medium text-[16px] text-[#666]">
+              We sent an SMS with a 6 digit code to your phone number. Please
+              type it below to complete deleting your account.
+            </div>
+          </div>
+
+          <div className="space-y-[20px]">
+            <div className="space-y-[8px]">
+              <div className="font-bold text-[14px] text-[#222]">
+                Verification Code
+              </div>
+              <input
+                className={filed}
+                placeholder="enter the verification code"
+                onChange={(e) => setVerificationCode(e.target.value)}
+                value={verificationCode}
+              />
+            </div>
+
+            <div
+              className={
+                isSecondStepInfoCompleted ? activeButton : inactiveButton
+              }
+              onClick={
+                isSecondStepInfoCompleted && isLoading === false
+                  ? secondStepHandler
+                  : undefined
+              }
+            >
+              {isLoading ? (
+                <UseAnimations animation={loading} size={24} color="222" />
+              ) : (
+                "Delete Your Account"
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  if (step === 3) {
+    return (
+      <div className="bg-[#fff] h-screen flex justify-center items-center">
+        <div className="font-extrabold text-[24px] text-[#222]">
+          Your account has been successfully deleted
+        </div>
+      </div>
+    );
   }
 };
 
