30a103cc7df84a35e7f74a54ba9804a50ce74167
2kram <al.awad7074@gmail.com>
2023-09-27 17:54:20 +0300

    support page

---
+++ frontend/website/app/support/page.tsx
@@ -0,0 +1,18 @@
+const Support = () => {
+  return (
+    <div className="bg-[#fff] p-[28px] md:p-[72px] lg:p-[0px] lg:flex lg:justify-center">
+      <div className="lg:w-[592px] lg:py-[72px] space-y-[12px]">
+        <div className="font-extrabold text-[24px] text-[#222] mb-[8px]">
+          Support
+        </div>
+
+        <div className="font-normal text-[#666] leading-8">
+          We whould be pleased to support you. Send us a message on WhatsApp at
+          our phone number +962790174799, or Email us at support@petlo.co.
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Support;
