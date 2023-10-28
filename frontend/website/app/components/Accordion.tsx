ebf06329edfebeb31afabdee28853f3aff6d89fa
2kram <al.awad7074@gmail.com>
2023-10-28 18:21:36 +0300

    things

--- frontend/website/app/components/Accordion.tsx
+++ frontend/website/app/components/Accordion.tsx
@@ -15,7 +15,7 @@ export const Accordion = ({
   const [openedIndex, setOpenedIndex] = useState<number>();
 
   return (
-    <div>
+    <div style={{ textAlign: "-webkit-center" }}>
       {items.map((item, index) => {
         const opened = openedIndex === index;
         const Icon = openedIndex === index ? ChevronUpIcon : ChevronDownIcon;
@@ -23,18 +23,18 @@ export const Accordion = ({
         return (
           <div
             key={index}
-            className="text-left py-[20px] border-b border-b-[#ddd]"
+            className="text-left py-[20px] border-b border-b-[#ddd] cursor-pointer lg:w-[800px]"
             onClick={() => setOpenedIndex(opened ? undefined : index)}
           >
             <div className="flex justify-between">
-              <div className="w-[75%] text-[#444] font-semibold text-[14px]">
+              <div className="w-[75%] text-[#444] font-semibold text-[14px] lg:text-[15px]">
                 {item.question}
               </div>
               <Icon width={18} height={18} strokeWidth={2.4} color="#444" />
             </div>
 
             {opened && (
-              <div className="mt-[20px] text-[14px] text-[#666]">
+              <div className="mt-[20px] text-[14px] leading-[24px] lg:text-[15px] lg:leading-[28px] font-medium  text-[#666]">
                 {reactStringReplace(item.answer, "{{br}}", (_, i) => (
                   <Fragment key={index}>
                     <br />
