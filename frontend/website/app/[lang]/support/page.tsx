74810adb16ce31403862429349f3daf6af05923a
2kram <al.awad7074@gmail.com>
2023-11-05 18:28:06 +0300

    Support Translation

--- frontend/website/app/[lang]/support/page.tsx
+++ frontend/website/app/[lang]/support/page.tsx
@@ -1,14 +1,21 @@
-const Support = () => {
+import { getTranslation } from "@/app/utils/getTranslation";
+
+const Support = async ({
+  params: { lang },
+}: {
+  params: { lang: "en" | "ar" };
+}) => {
+  const t = await getTranslation(lang);
+
   return (
-    <div className="bg-[#fff] p-[28px] md:p-[72px] lg:p-[0px] lg:flex lg:justify-center">
+    <div className="bg-[#fff] p-[28px] md:p-[72px] lg:p-[0px] lg:flex lg:justify-center h-[100%]">
       <div className="lg:w-[592px] lg:py-[72px] space-y-[12px]">
         <div className="font-extrabold text-[24px] text-[#222] mb-[8px]">
-          Support
+          {t["SUPPORT.TITLE"]}
         </div>
 
         <div className="font-normal text-[#666] leading-8">
-          We whould be pleased to support you. Send us a message on WhatsApp at
-          our phone number +962790174799, or Email us at support@petlo.co.
+          {t["SUPPORT.DESCRIPTION"]}
         </div>
       </div>
     </div>
