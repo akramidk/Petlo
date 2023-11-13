eb93f704c46633b1649d73b3ee5f1b16aba5658d
2kram <al.awad7074@gmail.com>
2023-11-13 17:11:49 +0300

    removed unwanted things from home page

--- frontend/mobile/app/(home)/index.tsx
+++ frontend/mobile/app/(home)/index.tsx
@@ -6,14 +6,9 @@ import {
   useInternationalizationContext,
   useTranslationsContext,
 } from "../../src/hooks";
-import {
-  BannersRequest,
-  BannersResponse,
-  SectionsResponse,
-} from "../../src/interfaces";
+import { BannersRequest, BannersResponse } from "../../src/interfaces";
 import { Loading } from "../../src/components/pages";
 import Scrollable from "../_Scrollable";
-import Section from "./_components/Seection";
 import Banners from "./_components/Banners";
 import { Warning } from "../../src/components/molecules";
 import { useRouter } from "expo-router";
@@ -51,22 +46,6 @@ const Home = () => {
     },
   });
 
-  const { response: sectionsResponse, mutate: mutateSections } = useAPIFetching<
-    void,
-    SectionsResponse
-  >({
-    endpoint: Endpoints.SECTIONS,
-    SWROptions: {
-      revalidateIfStale: false,
-    },
-    options: {
-      // TODO ther's a problem that the home page is rendered then
-      // the user got redirct to right page by RoutesRestrictor
-      // this should be fix
-      wait: !storedLanguage || (!customer && !skipCustomer),
-    },
-  });
-
   useEffect(() => {
     (async () => {
       if (
@@ -75,7 +54,6 @@ const Home = () => {
         ) === true
       ) {
         mutateBanners();
-        mutateSections();
         await AsyncStorage.removeItem(StorageKeys.REVALIDATE_HOME_PAGE_DATA);
       }
     })();
@@ -88,8 +66,6 @@ const Home = () => {
     (!customer && !skipCustomer) ||
     bannersResponse === undefined ||
     bannersResponse.isFetching ||
-    sectionsResponse === undefined ||
-    sectionsResponse.isFetching ||
     isCheckingRevalidationFinished === false
   ) {
     return <Loading />;
@@ -110,32 +86,6 @@ const Home = () => {
         {bannersResponse.body.data.length > 0 && (
           <Banners data={bannersResponse.body.data} />
         )}
-      </View>
-
-      <View className="mx-[28px]">
-        <Warning
-          firstText={t("HOME__WARNING_1_FIRST_TEXT")}
-          secondText={t("HOME__WARNING_1_SECOND_TEXT")}
-          onClick={() => router.push("/request-a-product")}
-        />
-
-        <View className="h-[4px]" />
-
-        <Warning
-          firstText={t("HOME__WARNING_2_FIRST_TEXT")}
-          secondText={t("HOME__WARNING_2_SECOND_TEXT")}
-          onClick={() => router.push("/support-and-feedbacks")}
-        />
-      </View>
-
-      <View className="space-y-[28px]">
-        {sectionsResponse?.body?.data?.map((section, i) => {
-          return (
-            <View key={i}>
-              <Section {...section} />
-            </View>
-          );
-        })}
       </View>
     </Scrollable>
   );
