bae049305a4ebdf144062c00a141a0e8b41ee9b3
2kram <al.awad7074@gmail.com>
2023-11-13 21:52:42 +0300

    add Shop page

--- frontend/mobile/app/shop/index.tsx
+++ frontend/mobile/app/shop/index.tsx
@@ -1,12 +1,14 @@
+import { useRouter } from "expo-router";
 import { View } from "react-native";
-import { Text } from "../../src/components/atoms";
+import { PageStructure } from "../../src/components/organisms";
+import { useTranslationsContext } from "../../src/hooks";
+import { CopyPhoneNumberButton } from "../_CopyPhoneNumberButton";
 
 const Shop = () => {
-  return (
-    <View>
-      <Text>hi</Text>
-    </View>
-  );
+  const router = useRouter();
+  const { t } = useTranslationsContext();
+
+  return <PageStructure title="Shop" backButton={router.back} />;
 };
 
 export default Shop;
