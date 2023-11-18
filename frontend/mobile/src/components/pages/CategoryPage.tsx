7dafde1ea399da046f2549210fff818d9cd4d0ec
2kram <al.awad7074@gmail.com>
2023-11-18 09:47:17 +0300

    CategoryPage add categories

--- frontend/mobile/src/components/pages/CategoryPage.tsx
+++ frontend/mobile/src/components/pages/CategoryPage.tsx
@@ -1,5 +1,10 @@
+import { useMemo, useState } from "react";
 import { View } from "react-native";
+import { Endpoints } from "../../enums";
+import { useAPIFetching } from "../../hooks";
+import { CategoriesResponse } from "../../interfaces";
 import PageStructure from "../organisms/PageStructure";
+import Tabs from "../organisms/Tabs";
 
 interface CategoryPage {
   publicId: string;
@@ -7,8 +12,48 @@ interface CategoryPage {
   backButton: () => void;
 }
 
+const ALL_CATEGORY = {
+  name: "All",
+  public_id: "ALL",
+};
+
 const CategoryPage = ({ publicId, name, backButton }: CategoryPage) => {
-  return <PageStructure title={name} backButton={backButton}></PageStructure>;
+  const [selectedCategory, setSelectedCategory] = useState(ALL_CATEGORY);
+  const { response: categoriesResponse } = useAPIFetching<
+    unknown,
+    CategoriesResponse
+  >({
+    endpoint: Endpoints.CATEGORIES,
+  });
+
+  const categoriesData = useMemo(() => {
+    if (!categoriesResponse?.body?.data) return [];
+
+    return [
+      ALL_CATEGORY,
+      ...categoriesResponse.body.data.filter(
+        (category) => category.parent_public_id === publicId
+      ),
+    ];
+  }, [categoriesResponse]);
+
+  return (
+    <PageStructure
+      title={name}
+      backButton={backButton}
+      HelperComponent={
+        <Tabs
+          data={categoriesData}
+          showSkeleton={categoriesData.length === 0}
+          selectedTab={selectedCategory}
+          onTabClick={(tab) => {
+            setSelectedCategory(tab);
+            //reset();
+          }}
+        />
+      }
+    ></PageStructure>
+  );
 };
 
 export default CategoryPage;
