28e3dda4cb9fc316f75e4c7d17cb8bc48f7ce966
2kram <al.awad7074@gmail.com>
2023-11-18 17:00:22 +0300

    MoreButton

--- frontend/mobile/src/components/atoms/MoreButton.tsx
+++ frontend/mobile/src/components/atoms/MoreButton.tsx
@@ -1,15 +1,20 @@
 import BaseButton from "../bases/BaseButton";
 import Text from "../atoms/Text";
+import clsx from "clsx";
 
 interface MoreButton {
   onClick: () => void;
   value: string;
+  cn?: string;
 }
 
-const MoreButton = ({ onClick, value }) => {
+const MoreButton = ({ onClick, value, cn }: MoreButton) => {
   return (
     <BaseButton
-      cn="w-[100%] bg-[#f6f6f6] rounded-[4px] items-center justify-center p-[12px]"
+      cn={clsx(
+        "w-[100%] bg-[#f6f6f6] rounded-[4px] items-center justify-center p-[12px]",
+        cn
+      )}
       onClick={onClick}
     >
       <Text cn="text-[14px] text-[#333]" font="bold">
