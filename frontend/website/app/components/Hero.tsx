1ce2b3ee555595ff93af761b0c652f880b643858
2kram <al.awad7074@gmail.com>
2023-12-05 18:43:56 +0300

    hero md

--- frontend/website/app/components/Hero.tsx
+++ frontend/website/app/components/Hero.tsx
@@ -16,20 +16,23 @@ export const Hero = ({ t, lang }: { t: any; lang: "en" | "ar" }) => {
     : ["ios", "android"];
 
   return (
-    <div className="bg-[#fff] w-[100%] py-[20px] space-y-[36px]">
-      <div dir="ltr" className="flex justify-between items-center px-[20px]">
+    <div className="bg-[#fff] w-[100%] py-[20px] md:py-[36px] space-y-[36px]">
+      <div
+        dir="ltr"
+        className="flex justify-between items-center px-[20px] md:px-[48px]"
+      >
         <Logo />
         <ChangeLangButton lang={lang} value={t["HOME.CHANGE_LANG"]} />
       </div>
 
-      <div className="px-[20px]">
-        <div className="text-center space-y-[16px]">
+      <div className="px-[20px] md:px-[48px]">
+        <div className="text-center space-y-[16px] md:space-y-[24px]">
           <div className="space-y-[12px]">
-            <div className="text-[24px] text-[#222] font-extrabold content-['\n'] whitespace-pre">
+            <div className="text-[24px] md:text-[36px] text-[#222] font-extrabold content-['\n'] whitespace-pre">
               {t["HOME.HERO.BIG_TEXT"]}
             </div>
 
-            <div className="font-medium text-[14px] text-[#777] leading-[22px] px-[12px]">
+            <div className="font-medium text-[14px] md:text-[16px] text-[#777] leading-[22px] px-[12px]">
               {t["HOME.HERO.TEXT"]}
             </div>
           </div>
