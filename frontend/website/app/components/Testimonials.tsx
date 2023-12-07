85167694ec9dfe8a50fea42d3816bec415a02336
2kram <al.awad7074@gmail.com>
2023-12-07 16:21:45 +0300

    things

--- frontend/website/app/components/Testimonials.tsx
+++ frontend/website/app/components/Testimonials.tsx
@@ -5,7 +5,7 @@ export const Testimonials = ({ t, lang }: { t: any; lang: "en" | "ar" }) => {
   return (
     <div className="bg-[#fff] p-[20px] md:p-[48px] lg:p-[72px]">
       <div className="text-center text-[#222] text-[24px] font-extrabold mb-[36px] px-[20%]">
-        What Petloers Said about Us
+        {t["HOME.TESTIMONIALS.TITLE"]}
       </div>
 
       <div className="grid md:grid-cols-2 xl:grid-cols-4 gap-[20px]">
@@ -25,7 +25,7 @@ export const Testimonials = ({ t, lang }: { t: any; lang: "en" | "ar" }) => {
 
         <div className="bg-[#EBE3D5] rounded-[16px] p-[52px] md:h-[700px] w-[100%] flex items-center justify-center flex-col space-y-[16px]">
           <div className="text-[#222] font-bold text-[20px] text-center">
-            Download The App and Be The Next Petloer!
+            {t["HOME.TESTIMONIALS.TEXT"]}
           </div>
 
           <DownloadButtons ignoreFlex t={t} lang={lang} borderColor="#e2d6c1" />
