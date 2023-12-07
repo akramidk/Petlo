0e63ffbb0a1b4f7c68bb8392f218bf117a1855c8
2kram <al.awad7074@gmail.com>
2023-12-07 17:08:07 +0300

    fixed pet add scope customer id

--- backend/app/models/pet.rb
+++ backend/app/models/pet.rb
@@ -7,7 +7,7 @@ class Pet < ApplicationRecord
 
   validates :public_id, presence: { message: 2001000 }, uniqueness: { message: 2001001 }
   validates :customer_id, presence: { message: 2001002 }
-  validates :name, presence: { message: 2001003 }, uniqueness: { scope: [:kind, :breed, :gender], message: 2001004 }
+  validates :name, presence: { message: 2001003 }, uniqueness: { scope: [:kind, :breed, :gender, :customer_id], message: 2001004 }
   validates :kind, presence: { message: 2001005 }, inclusion: { in: CONSTANTS::PETS.keys, message: 2001006 }
   #TODO should be without transform_keys
   validates :breed, presence: { message: 2001007 }, inclusion: { in: CONSTANTS::PETS["dog"][:breeds].transform_keys(&:to_s).keys, message: 2001008 }, if: -> { kind == "dog" }
