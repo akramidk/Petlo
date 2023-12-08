9d7288daff6c9af8e9c3892118378d4c97081170
2kram <al.awad7074@gmail.com>
2023-12-08 18:30:26 +0300

    HowItWorks 6

--- frontend/website/app/components/HowItWorks.tsx
+++ frontend/website/app/components/HowItWorks.tsx
@@ -3,6 +3,10 @@ import Image from "next/image";
 export const HowItWorks = ({ t, lang }: { t: any; lang: "en" | "ar" }) => {
   return (
     <div className="bg-[#f9f9f9] p-[20px] md:p-[48px] lg:p-[72px]">
+      <div className="text-center text-[#222] text-[24px] font-extrabold mb-[36px] px-[20%]">
+        {t["HOME.HOW_IT_WORKS.TITLE"]}
+      </div>
+
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-[20px]">
         {Array.apply(null, Array(6)).map((_, index) => {
           return (
