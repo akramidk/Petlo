f2a963465aad9c9889aa19d03bedf589205bb0c3
2kram <al.awad7074@gmail.com>
2023-12-09 02:21:06 +0300

    Last 3

--- frontend/website/app/components/Last.tsx
+++ frontend/website/app/components/Last.tsx
@@ -2,7 +2,7 @@ import { Accordion } from "./Accordion";
 
 export const Last = ({ t, lang }: { t: any; lang: "en" | "ar" }) => {
   return (
-    <div className="bg-[#f9f9f9] p-[20px] md:p-[48px] lg:p-[72px] flex flex-col items-center justify-center space-y-[20px]">
+    <div className="bg-[#f9f9f9] p-[20px] md:p-[48px] lg:p-[72px] flex flex-col items-center justify-center space-y-[72px]">
       <div>
         <div className="text-center text-[#222] text-[24px] font-extrabold mb-[56px]">
           {t["HOME.LAST.TITLE"]}
@@ -21,6 +21,10 @@ export const Last = ({ t, lang }: { t: any; lang: "en" | "ar" }) => {
           lang={lang}
         />
       </div>
+
+      {false && (
+        <div className="bg-[#fff] h-[52px] w-[100%] rounded-[16px]">dd</div>
+      )}
     </div>
   );
 };
