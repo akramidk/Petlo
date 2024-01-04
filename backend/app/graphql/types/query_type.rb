6a71a95a58a75390f033594b83992b36d4bf7fa0
2kram <al.awad7074@gmail.com>
2024-01-04 20:57:54 +0300

    filter

--- backend/app/graphql/types/query_type.rb
+++ backend/app/graphql/types/query_type.rb
@@ -1,5 +1,7 @@
 module Types
   class QueryType < Types::BaseObject
+    require 'json'
+
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
@@ -7,20 +9,21 @@ module Types
     field :items, [Types::ItemType], null: false do
       argument :page, Int, required: false
       argument :limit, Int, required: false
+      argument :filter, GraphQL::Types::JSON, required: false
       argument :order, String, required: false
     end
-    def items(page: nil, limit: nil, order: nil)
-      query(Item, page, limit, order)
+    def items(page: nil, limit: nil, filter: nil, order: nil)
+      query(Item, page, limit, filter, order)
     end
 
-
     private
-    def query(entity, page, limit, order)
+    def query(entity, page, limit, filter, order)
       page = page || 1
       limit = limit || 100
       order = order || "asc"
+      filter = filter || ""
 
-      entity.all.order(id: order).limit(limit).offset((page * limit) - limit)
+      entity.joins(:details).where(filter).order(id: order).limit(limit).offset((page * limit) - limit)
     end
   end
 end
