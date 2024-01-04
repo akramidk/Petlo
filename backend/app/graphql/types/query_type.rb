e3007a5484655524e42036d98845048c7d7859d0
2kram <al.awad7074@gmail.com>
2024-01-04 16:32:15 +0300

    added order

--- backend/app/graphql/types/query_type.rb
+++ backend/app/graphql/types/query_type.rb
@@ -7,18 +7,20 @@ module Types
     field :items, [Types::ItemType], null: false do
       argument :page, Int, required: false
       argument :limit, Int, required: false
+      argument :order, String, required: false
     end
-    def items(page: nil, limit: nil)
-      query(Item, page, limit)
+    def items(page: nil, limit: nil, order: nil)
+      query(Item, page, limit, order)
     end
 
 
     private
-    def query(entity, page, limit)
+    def query(entity, page, limit, order)
       page = page || 1
       limit = limit || 100
+      order = order || "asc"
 
-      entity.all.limit(limit).offset((page * limit) - limit)
+      entity.all.order(id: order).limit(limit).offset((page * limit) - limit)
     end
   end
 end
