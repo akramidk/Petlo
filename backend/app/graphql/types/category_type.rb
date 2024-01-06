dd53a1e8785492a47d3f57acc99ce9b9ba14656d
2kram <al.awad7074@gmail.com>
2024-01-06 18:20:55 +0300

    types

--- backend/app/graphql/types/category_type.rb
+++ backend/app/graphql/types/category_type.rb
@@ -1,5 +1,6 @@
 module Types
     class CategoryType < Types::BaseObject
+      field :id, Int, null: false
       field :public_id, String, null: false
       field :name, String, null: false
       field :parent_id, Int, null: true
