1797af5a1aabe6bcd9c49de00af40d63cee827e3
2kram <al.awad7074@gmail.com>
2024-02-05 15:02:44 +0300

    option value

--- backend/app/graphql/types/variant_option_type.rb
+++ backend/app/graphql/types/variant_option_type.rb
@@ -4,5 +4,10 @@ module Types
     field :variant_id, Int, null: false
     field :option_id, Int, null: false
     field :option_value_number, Int, null: false
+    field :value, String, null: false
+
+    def value
+      Option.find_by(id: object.option_id).values.find_by(number: object.option_value_number).value
+    end
   end
 end
